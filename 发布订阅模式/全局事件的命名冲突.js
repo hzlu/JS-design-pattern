0158d6eaac120381d53b8c878ba6e51dcd5b2fe3
haozhao <luhaozhao@luhaozhaodeMacBook-Pro.local>
2017-02-28 21:47:39 +0800

    更新发布订阅模式📒

--- 发布订阅模式/全局事件的命名冲突.js
+++ 发布订阅模式/全局事件的命名冲突.js
@@ -121,4 +121,21 @@ var Event = (function(){
   return Event;
 })();
 
+/************************* 先发布后订阅 **************************/
+Event.trigger('click', 1);
+
+Event.listen('click', function(a) {
+  console.log(a);
+});
+
+/************************* 使用命名空间 **************************/
+Event.create('namespace1').listen('click', function(a) {
+  console.log(a);
+});
+Event.create('namespace1').trigger('click', 1);
+
+Event.create('namespace2').listen('click', function(a) {
+  console.log(a);
+});
+Event.create('namespace2').trigger('click', 2);
 
